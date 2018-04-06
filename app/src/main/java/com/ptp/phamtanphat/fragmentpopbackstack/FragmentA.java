7295da5f74ba81e16edb3324f57dfa846730f948
thuviengithub0208 <khoaphp@yahoo.com>
2018-04-06 18:03:14 +0700

    "Update "

---
+++ app/src/main/java/com/ptp/phamtanphat/fragmentpopbackstack/FragmentA.java
@@ -0,0 +1,8 @@
+package com.ptp.phamtanphat.fragmentpopbackstack;
+
+/**
+ * Created by KhoaPhamPC on 6/4/2018.
+ */
+
+public class FragmentA {
+}
--- app/src/main/java/com/ptp/phamtanphat/fragmentpopbackstack/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/fragmentpopbackstack/MainActivity.java
@@ -2,12 +2,17 @@ package com.ptp.phamtanphat.fragmentpopbackstack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void AddFragmentA(View view){
+
     }
 }
