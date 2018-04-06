0de76d65d495c023303b6fed543ce8aed6a539f8
thuviengithub0208 <khoaphp@yahoo.com>
2018-04-06 18:11:28 +0700

    "Update "

--- app/src/main/java/com/ptp/phamtanphat/fragmentpopbackstack/FragmentB.java
+++ app/src/main/java/com/ptp/phamtanphat/fragmentpopbackstack/FragmentB.java
@@ -1,8 +1,20 @@
 package com.ptp.phamtanphat.fragmentpopbackstack;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by KhoaPhamPC on 6/4/2018.
  */
 
-public class FragmentB {
+public class FragmentB extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_b,container,false);
+    }
 }
